7d920d25a4bf315a7278b5de0413857d94d25955
Rashid Schami <rashidschami@RashidSchamiMBP.fritz.box>
2019-07-19 21:22:36 +0200

    Add basic functions for bq operations

---
+++ bq_toolbelt.py
@@ -0,0 +1,41 @@
+from google.cloud import bigquery
+from datetime import datetime
+from helper import repeat_function_for_daterange
+
+
+client = bigquery.Client.from_service_account_json(
+    'bq_service_account/bq_service_account.json')
+
+
+def copy_table(date_suffix, source_project, source_dataset, source_table,
+               dest_project, dest_dataset, dest_table):
+    source_table += date_suffix
+    dest_table += date_suffix
+    source_table_ref = client.dataset(
+        source_dataset, project=source_project).table(source_table)
+    dest_table_ref = client.dataset(
+        dest_dataset, project=dest_project).table(dest_table)
+    job = client.copy_table(
+        source_table_ref,
+        dest_table_ref,
+        location='US')
+    print(f'''Send copy job for {source_project}.{source_dataset}.{source_table} to {dest_project}.{dest_dataset}.{dest_table} \n''')
+
+
+def delete_table(date_suffix, project, dataset, table):
+    table += date_suffix
+    table_ref = client.dataset(dataset, project=project).table(table)
+    client.delete_table(table_ref)
+    print(f'Send delete job for {project}.{dataset}.{table} \n')
+
+
+def write_query_result(date_suffix, sql_query, project, dataset, table, write_disposition='WRITE_EMPTY'):
+    table += date_suffix
+    table_ref = client.dataset(
+        dataset, project=project).table(table)
+    job_config = bigquery.QueryJobConfig()
+    job_config.destination = table_ref
+    job_config.write_disposition = write_disposition
+    query_job = client.query(sql_query, job_config=job_config)
+    query_job.result()
+    print(f'Query written to table: {project}.{dataset}.{table} \n')
---
+++ helper.py
@@ -0,0 +1,36 @@
+from datetime import timedelta
+from multiprocessing.dummy import Pool as ThreadPool
+import itertools
+import numpy
+
+
+def repeat_function_for_daterange(function, parameters, start_date, end_date, nr_threads=10):
+    dates = numpy.array_split(get_dates_for_daterange(start_date, end_date),
+                              nr_threads)
+    with ThreadPool(nr_threads) as pool:
+        pool.starmap(repeat_function_for_dates,
+                     zip(itertools.repeat(function),
+                         itertools.repeat(parameters),
+                         dates)
+                     )
+
+
+def get_dates_for_daterange(start_date, end_date):
+    return [(start_date + timedelta(i)) for i in range((end_date - start_date).days + 1)]
+
+
+def repeat_function_for_dates(function, parameters, dates):
+    for date in dates:
+        run_function_for_date(function, parameters, date)
+
+
+def run_function_for_date(function, parameters, date):
+    parameters['date_suffix'] = get_suffix_from_date(date)
+    try:
+        function(**parameters)
+    except Exception as e:
+        print(f'Error for {function.__name__}{parameters}: {e}')
+
+
+def get_suffix_from_date(date):
+    return f'{date.year}{date.month:02d}{date.day:02d}'
